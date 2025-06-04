a9490bdfa30dd92eaaf0c3cb5dd4207c8a9c4066
tylxzz <markbencevarga@gmail.com>
2025-06-04 03:00:32 +0200

    rendezes javitva

--- rendezes/manager.js
+++ rendezes/manager.js
@@ -19,9 +19,29 @@ class Manager {
         return [...this.#array]
     }
 
+    #honapToKey(honap) {
+        if (!honap) return ""
+        let key = honap.toLowerCase()
+            .replace(/[áäâà]/g, "a")
+            .replace(/[éëèê]/g, "e")
+            .replace(/[íïîì]/g, "i")
+            .replace(/[óöőôò]/g, "o")
+            .replace(/[úüűùû]/g, "u")
+        if (key.startsWith("sze")) return "szep"
+        return key.slice(0, 3)
+    }
+
     sortBy(field, order) {
+        const HONAPOK = ["jan","feb","mar","apr","maj","jun","jul","aug","szep","okt","nov","dec"]
         this.#array.sort((a, b) => {
             let va = a[field], vb = b[field]
+            if (field === "honap") {
+                const vaIndex = HONAPOK.indexOf(this.#honapToKey(va))
+                const vbIndex = HONAPOK.indexOf(this.#honapToKey(vb))
+                if (vaIndex < vbIndex) return order === "asc" ? -1 : 1
+                if (vaIndex > vbIndex) return order === "asc" ? 1 : -1
+                return 0
+            }
             if (typeof va === "string") {
                 va = va.toLowerCase()
                 vb = vb.toLowerCase()
